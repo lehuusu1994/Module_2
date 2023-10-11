b6fe0a35e810683fe292b0ab05741ff8db4cdafc
unknown <leesu031994@gmail.com>
2023-10-11 22:34:45 +0700

    greatest_common_factor

---
+++ lesson-02-java-loop/src/com/codegym/greatest_common_factor/GreatestCommonFactor.java
@@ -0,0 +1,30 @@
+package com.codegym.greatest_common_factor;
+
+import java.util.Scanner;
+
+public class GreatestCommonFactor {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a:");
+        a = sc.nextInt();
+        System.out.println("Enter b:");
+        b = sc.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a ==0 || b ==0){
+            System.out.println("No greatest common factor !");
+        }
+        while (a != b){
+            if (a > b) {
+                a = a -b;
+            } else {
+                b = b-a;
+            }
+        }
+        System.out.println("Greatest common factor: "+ a);
+    }
+}
