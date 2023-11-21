91d4481941fdc7fb565323ce713fe6d1a826fafd
unknown <leesu031994@gmail.com>
2023-11-21 10:47:13 +0700

    circle_cylinder

---
+++ lesson-06-Inheritance/src/circle_cylinder/Test.java
@@ -0,0 +1,12 @@
+package circle_cylinder;
+
+public class Test {
+    public static void main(String[] args) {
+        Circle circle = new Circle(10,"Yellow");
+        System.out.println(circle);
+
+        Cylinder cylinder = new Cylinder(10,"Pink",10);
+        System.out.println(cylinder);
+
+    }
+}
